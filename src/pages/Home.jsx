636f07951efddd59ead6d6f8d8e6d6312bbcaff1
David Duarte (Williams) <davidduart04@gmail.com>
2026-03-07 18:29:03 -0300

    novo layout inicial básico, ainda requer um charme a mais...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,64 @@
 import './styles/home.css'
 import InstagramLogo from '../assets/instagram_logo.png'
 import WhatsappLogo from '../assets/whatsapp_logo.png'
+import ImagemLonga from '../assets/fotos/jardinNoite.jpeg'
+import WMPlogo from '../assets/WMPlogo.svg'
+import Secao2_1 from '../assets/fotos/secao2-1.jpeg'
+import Secao2_2 from '../assets/fotos/secao2-2.jpeg'
+import Secao2_3 from '../assets/fotos/secao2-3.jpeg'
+import Secao2_4 from '../assets/fotos/secao2-4.jpeg'
 
 export default function Home() {
     return (
         <>
             <main className="mainHome">
-                <header className='menuSuperior'>
-                    <div className='divAuxLogoSuperior'>
-                        logo
+                <section className='secao1'>
+                    <div className='esquerda'>
+                        <img src={WMPlogo} alt="" className='logoWMP' />
                     </div>
-                    <div className='divAuxRedes'>
-                        <a href="" className='redesSociaisLink'>
-                            <img src={InstagramLogo} alt="" className='redeSocialLogo' />
-                        </a>
-                        <a href="" className='redesSociaisLink'>
-                            <img src={WhatsappLogo} alt="" className='redeSocialLogo' />
-                        </a>
+                    <div className='direita'>
+                        <h1 className='titulo'>
+                            Transformamos o seu espaço com qualidade e confiança
+                        </h1>
+                        <h2 className='subtitulo'>
+                            Cuidamos de cada detalhe da sua obra ou reforma
+                        </h2>
                     </div>
-                </header>
+                </section>
+                <div className='divisaoSecao1'>
+                    <h2>Alguns de nossos serviços</h2>
+                </div>
+                <div className='secao2'>
+                    <div className='imagemLonga'>
+                        <img src={ImagemLonga} alt="" />
+                    </div>
+                    <div className='direita'>
+                        <div className='blocoItemServico'>
+                            <div className='auxImagem'>
+                                <img src={Secao2_1} alt="" />
+                            </div>
+                            <p><b>Instalação Elétrica Predial e Industrial</b></p>
+                        </div>
+                        <div className='blocoItemServico'>
+                            <div className='auxImagem'>
+                                <img src={Secao2_2} alt="" />
+                            </div>
+                            <p><b>Áreas externas e Paisagismo</b></p>
+                        </div>
+                        <div className='blocoItemServico'>
+                            <div className='auxImagem'>
+                                <img src={Secao2_3} alt="" />
+                            </div>
+                            <p><b>Iluminação com Dimmer</b></p>
+                        </div>
+                        <div className='blocoItemServico'>
+                            <div className='auxImagem'>
+                                <img src={Secao2_4} alt="" />
+                            </div>
+                            <p><b>Acabamento de Paredes</b></p>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     )
